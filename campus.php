174390fe59e169c5ffdc52c861fa13b19cf4454e
night18 <warrior.sp@gmail.com>
2017-04-08 17:45:17 -0400

    first commit

---
+++ campus.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Parking</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<div class="inheader_pic"><img src="logo.png" height="80" width="80" id="logo"></div>
+		<div class="inheader_text"><h2>West Virginia Univeristy</h2></div>
+	</div>
+	<div class="content">
+		<button type="button" onclick="redirect()" >Login!</button>
+	</div>
+
+	<script type="text/javascript">
+		function redirect() {
+			window.location.href = "./campus.php";
+		}
+	</script>
+</body>
+</html>
